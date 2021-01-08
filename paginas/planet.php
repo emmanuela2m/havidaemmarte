a6d36ff39a1ead41e1149f9de6fc72cd923489a2
PROGR WEB - MANHA <PROGR WEB - MANHA@MININT-U2SA7DV>
2021-01-08 11:47:19 -0300

    djhhfhsfhfvhfvjhfjihe

---
+++ paginas/planet.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" >
+    <link rel="stylesheet" href="plugins/planet/dist/style.css"
+    <head>
+        <meta charset="UTF-8">
+        <title>CodePen - Earth planet</title>
+        <link rel="stylesheet" href="./style.css">
+
+    </head>
+    <body>
+        <!-- partial:index.partial.html -->
+        <div class="earth"></div>
+        <!-- partial -->
+
+    </body>
+</html>
